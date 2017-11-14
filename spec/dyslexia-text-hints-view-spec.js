08ac7676cb97fe8859d64194ec5de39471b28f4e
Samuel Russell <sr14978@bristol.ac.uk>
2017-11-14 11:54:07 +0000

    initial default app

---
+++ spec/dyslexia-text-hints-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DyslexiaTextHintsView from '../lib/dyslexia-text-hints-view';
+
+describe('DyslexiaTextHintsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
